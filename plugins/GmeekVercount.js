fdb4074f55c57f4399eddc30b9ce2a7a1d15f5d2
TodoMy <62632079+todomy@users.noreply.github.com>
2025-05-22 18:25:05 +0800

    Update GmeekVercount.js

--- plugins/GmeekVercount.js
+++ plugins/GmeekVercount.js
@@ -12,5 +12,4 @@ document.addEventListener("DOMContentLoaded", function() {
     var element = document.createElement('script');
     element.src = 'https://vercount.one/js';
     document.head.appendChild(element);
-    console.log("\n %c GmeekVercount Plugins https://github.com/todomy/TodoMy.github.io \n","padding:5px 0;background:#bc4c00;color:#fff");
 });
